be4255d40afd86e4b847ed2ea09cbe30f30037b0
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-11 03:00:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -47,22 +47,20 @@ monopoly = {
       :reading_railroad => 
       {
         "mortgage_value" => "$100"
-        
       }, 
       :pennsylvania_railroad => 
       {
         "mortgage_value" => "$200"
-        
       }, 
       :b_and_o_railroad => 
       {
         "mortgage_value" => "$400"
-        
       }, 
       :shortline_railroad => 
       {
         "mortgage_value" => "$800"
-  
-      }}}
+      }
+    }
+  }
   }
 end
